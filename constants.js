f74e243ac664e1b4ed8d6e66b0b24630249ec911
Novakovskiy Rodion <novakovskiy.ra@outlook.com>
2025-04-21 20:30:48 +0300

    Including API key into environment file

--- constants.js
+++ constants.js
@@ -2,5 +2,6 @@ module.exports = {
     DB_HOST: process.env.DB_HOST,
     DB_USER: process.env.DB_USER,
     DB_PASSWORD: process.env.DB_PASSWORD,
-    SECRET_KEY: process.env.SECRET_KEY
+    SECRET_KEY: process.env.SECRET_KEY,
+    API_KEY: process.env.API_KEY
 }
