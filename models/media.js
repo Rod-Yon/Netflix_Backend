36ab50b7adae9826e0559b59e06b6c955d8be532
Novakovskiy Rodion <novakovskiy.ra@outlook.com>
2025-04-22 15:39:19 +0300

    Creation of "Media" model

---
+++ models/media.js
@@ -0,0 +1,12 @@
+const { Schema, model } = require('mongoose');
+
+const media_schema = new Schema({
+    id: { type: Number, required: true },
+    type: { type: String, required: true, enum: ['Movie', 'Show'] },
+    name: { type: String, required: true },
+    reviews: { type: [{ type: Schema.Types.ObjectId, ref: 'Review' }], required: true, default: [] }
+}, { collection: "Medias" });
+
+const Media = model('Media', media_schema);
+
+module.exports = Media;
