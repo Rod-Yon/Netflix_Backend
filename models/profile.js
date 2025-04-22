ef13769b27c3d13ef10d59471566ca79a9c8295b
Novakovskiy Rodion <novakovskiy.ra@outlook.com>
2025-04-22 15:40:47 +0300

    Updating "Profile" model

--- models/profile.js
+++ models/profile.js
@@ -2,7 +2,10 @@ const { Schema, model } = require('mongoose');
 
 const profile_schema = new Schema({
     name: { type: String, required: true },
-    avatar: { type: String, required: true }
+    avatar: { type: String, required: true },
+    preferences: { type: Map, of: Number, default: {}, required: true },
+    wishlist: { type: [{ type: Schema.Types.ObjectId, ref: 'Media' }], required: true, default: [] },
+    reviews: { type: [{ type: Schema.Types.ObjectId, ref: 'Review' }], required: true, default: [] }
 }, { collection: "Profile" });
 
 const Profile = model('Profile', profile_schema);
