e106f144e1f6ec69b65a612d0dddc5f07ed445fb
Novakovskiy Rodion <novakovskiy.ra@outlook.com>
2025-04-22 15:38:56 +0300

    Creation of "Review" model

---
+++ models/review.js
@@ -0,0 +1,14 @@
+const { Schema, model } = require('mongoose');
+
+const review_schema = new Schema({
+    profile: { type: { type: Schema.Types.ObjectId, ref: 'Profile' }, required: true, },
+    review_type: { type: String, required: true, enum: ['Private', 'Public'] },
+    media_type: { type: String, required: true, enum: ['Movie', 'Show'] },
+    media_id: { type: Number, required: true },
+    rating: { type: Number, required: true },
+    review: { type: String, required: true,}
+}, { collection: "Reviews" });
+
+const Review = model('Review', review_schema);
+
+module.exports = Review;
