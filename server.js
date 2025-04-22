5af0037c41f59591aec401c5ac5b762cde3e8f46
Novakovskiy Rodion <novakovskiy.ra@outlook.com>
2025-04-22 15:44:05 +0300

    Including authorization router into server

--- server.js
+++ server.js
@@ -1,14 +1,43 @@
 const express = require('express');
 const logger = require('morgan');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookie_parser = require('cookie-parser');
+
 
 const port = process.env.PORT || 8080;
 
+const { authorization_router } = require('./routers/authorization_router');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger('dev'));
+app.use(cookie_parser());
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
+
+app.use((req, res, next) => {
+
+    if (req.path == '/') return next();
+
+    const token = req.cookies?.token;
+    if (!token) return res.redirect('/');
+
+    try {
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);
+        req.user = decoded;
+        next();
+    } catch {
+        return res.redirect('/');
+    }
+});
+
+app.use('/', authorization_router);
+
 
 
 app.listen(port, () => {
