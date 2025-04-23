7f9d2fdfd4e7910e96a71cd2e3472ab38dade49d
Novakovskiy Rodion <novakovskiy.ra@outlook.com>
2025-04-24 00:02:00 +0300

    Updating "Media" model

--- models/media.js
+++ models/media.js
@@ -1,9 +1,11 @@
 const { Schema, model } = require('mongoose');
 
 const media_schema = new Schema({
-    id: { type: Number, required: true },
+    tmdb_id: { type: Number, required: true },
     type: { type: String, required: true, enum: ['Movie', 'Show'] },
     name: { type: String, required: true },
+    poster_path: { type: String, required: true },
+    description: { type: String, required: true },
     reviews: { type: [{ type: Schema.Types.ObjectId, ref: 'Review' }], required: true, default: [] }
 }, { collection: "Medias" });
 
